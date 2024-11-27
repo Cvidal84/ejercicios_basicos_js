1ee4bda008e5672304bc0292b59753272c48d2a6
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2024-11-27 22:49:18 +0100

    ejercicios
    ejercicios poco a poco

---
+++ Ejercicio1.js
@@ -0,0 +1,15 @@
+//1 variable mi super heroe favorito
+let miSuperheroeFavorito = "Hulk"
+//2 variable numero favorito
+let numeroFavorito = 50
+//3 dos variables numericas
+let altura = 5
+let longitud = 10
+//4 suma de dos variables
+let suma = altura + longitud
+//5 mostrar todo por pantalla
+console.log(miSuperheroeFavorito)
+console.log(numeroFavorito)
+console.log(altura)
+console.log(longitud)
+console.log(suma)
---
+++ Ejercicio2.js
@@ -0,0 +1,38 @@
+//cambia la edad de luke de 19 a 25 años
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+//opción1
+jedi.edad = 25
+console.log(jedi)
+//opcion2
+jedi["edad"] = 25
+console.log(jedi)
+//parte 2 del ejercicio
+//Crea tres variables y muestra un mensaje por pantalla
+let nombre = "Leia"
+let apellido = "Organa"
+let edad = 20
+console.log("Soy "+nombre+" "+apellido+", tengo "+edad+" años y soy una princesa de Alderaan.")
+//parte 3 del ejercicio
+//Calcula el precio total de los dos sables
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+// Tu código para calcular el precio total aquí...
+let total = sable1.precio + sable2.precio
+console.log("El precio de los dos sables es: "+total)
+//parte 4 del ejercicio
+let precioBaseGlobal = 10000;
+
+// Cambia el precio base en esta linea modificando la variable
+precioBaseGlobal = 25000;
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+// Tu código para actualizar el precio final de cada nave aquí...
+nave1.precioFinal = precioBaseGlobal + nave1.precioBase;
+nave2.precioFinal = precioBaseGlobal + nave2.precioBase;
+console.log(nave1);
+console.log(nave2);
+
+
