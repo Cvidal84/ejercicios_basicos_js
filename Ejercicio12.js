fde8af6226ed59cee05c17e9d6cf75b65a312bee
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2024-12-17 00:38:47 +0100

    Update Ejercicio12.js

--- Ejercicio12.js
+++ Ejercicio12.js
@@ -11,23 +11,9 @@ const duplicates = [
     'pasta',
     'soda'
   ];
-  //forma de hacerlo 1, esta no la entiendo bien.
-  function removeDuplicates(list) {
-    return list.filter((value, index, self) => self.indexOf(value) === index) //este he tenido que buscar por internet, me cuesta entenderlo. 
-  }
-
- const noduplicates = removeDuplicates(duplicates)
- console.log(noduplicates)
-
- //forma de hacerlo 2, me parece más fácil
- function removeDuplicates2(list) {
-  const removeDup = new Set(list)
-  return removeDup
- }
- console.log(removeDuplicates2(duplicates))
-
- //antonio, lo encaminó así en clase.
- function removeDuplicates3(list) {
+  
+//se crea una nueva array, usamos un for of para meter los items que no esten en uniques, si estan no se meten.
+ function removeDuplicates(list) {
   const uniques = []
   for (const item of list) {
     if (!uniques.includes(item)) {
@@ -36,4 +22,4 @@ const duplicates = [
   }
   return(uniques)
  }
- console.log(removeDuplicates3(duplicates))
+ console.log(removeDuplicates(duplicates))
